d806238f56a3156cacc9b3dc2bcd8b1589ae04fd
Wojtazooo <wojtekulaski@gmail.com>
2020-12-09 00:39:46 +0100

    add day1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Advent_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int>();
+            string file_name = "text.txt";
+            if (!File.Exists(file_name))
+            {
+                Console.WriteLine("file not exists");
+                return;
+            }
+            string[] lines = File.ReadAllLines(file_name);
+
+            foreach(string s in lines)
+            {
+                values.Add(int.Parse(s));
+            }
+
+            for(int i = 0; i < values.Count; i++) // o(n^2)
+            {
+                for(int j = i; j < values.Count; j++)
+                {
+                    if (values[i] + values[j] == 2020)
+                    {
+                        Console.WriteLine($"{values[i]} + {values[j]} = 2020");
+                        Console.WriteLine($"{values[i]} * {values[j]} = {values[i] * values[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
