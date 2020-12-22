0844ea1388209c31c5a0ca3a75615d51e2a08685
Wojtazooo <wojtekulaski@gmail.com>
2020-12-22 11:07:55 +0100

    add next_question method to day 8 part one

--- Day8_1.cs
+++ Day8_1.cs
@@ -7,6 +7,24 @@ namespace Advent_of_Code
 {
     class Program
     {
+        static (int n_position, int n_acc) next_command(string command, int acc, int pos)
+        { 
+            // read value
+            Regex rx_value = new Regex(@"[+-][0-9]+");
+            string s_value = rx_value.Match(command).ToString();
+            int value = int.Parse(s_value.Substring(1));
+            if (s_value[0] == '-')
+                value *= -1;
+
+            if (command.Contains("nop")) 
+                return (pos + 1, acc);
+            else if (command.Contains("acc"))
+                return (pos + 1, acc + value);
+            else if (command.Contains("jmp"))
+                return (pos + value, acc);
+            else return (0, 0); // error
+        }
+
         static void Main(string[] args)
         {
             // read file
@@ -19,31 +37,23 @@ namespace Advent_of_Code
             string[] lines = File.ReadAllLines(file_name);
 
 
-            Regex rx_value = new Regex(@"[+-][0-9]+");
+            
             HashSet<int> executed = new HashSet<int>();
 
             int acc = 0;
-            for(int i = 0; i < lines.Length;i++)
+            int last = 0;
+            for(int i = 0; i < lines.Length;)
             {
                 Console.WriteLine(lines[i]);
-                if (executed.Add(i) == false) break;
-                if (lines[i].Contains("nop")) continue;
-                
-                // read value
-                string s_value = rx_value.Match(lines[i]).ToString();
-                int value = int.Parse(s_value.Substring(1));
-                if (s_value[0] == '-')
-                    value *= -1;
-                Console.WriteLine(value);
-
-                if (lines[i].Contains("acc"))
-                    acc += value;
-                else if (lines[i].Contains("jmp"))
-                    i += (value - 1);
+                if (executed.Add(i) == false) // line repeated
+                { 
+                    break;
+                }
+                last = i;
+                (i, acc) = next_command(lines[i], acc, i);               
             }
 
             Console.WriteLine("Acc = " + acc);
-
         }
     }
 }
